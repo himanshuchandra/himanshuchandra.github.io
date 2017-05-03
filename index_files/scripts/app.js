a23ca2af021b02ea9c090df63d9dc79c81a6e87c
Himanshu_Chandra <himanshu.chandra@outlook.com>
2017-05-03 21:32:25 +0530

    new resume initialized

---
+++ index_files/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name resumeApp
+ * @description
+ * # resumeApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('resumeApp', [
+    'ngAnimate'
+  ]);
---
+++ index_files/scripts/controllers/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name resumeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the resumeApp
+ */
+angular.module('resumeApp')
+  .controller('MainCtrl', function () {
+    
+  });
